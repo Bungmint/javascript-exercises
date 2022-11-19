ece926f38bd20af94f6b365ec80737709cb35cfb
Bungmint <youngminpark555@gmail.com>
2022-11-18 17:11:52 -0800

    Solve upto 07_tempConversion

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let res = "";
+    let n = string.length;
+    for (let i = n - 1; i >= 0; i--) {
+        res += string[i];
+    }
+    return res;
+    // string.split('').reverse().join('')
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  return Math.round(50 * (temp - 32) / 9) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  return Math.round(18 * temp + 320) / 10;
 };
 
 // Do not edit below this line
